8bf80166835b65bf72994f7ed0c22e2c40a3cf73
Thales Gelinger <thalesgelinger@gmail.com>
2024-06-20 20:59:27 -0300

    feat: remove target

--- packages/rover-mechanic/target/debug/deps/rover_mechanic-fd802ffb9f5f9087.d
+++
@@ -1,5 +0,0 @@
-/Users/thalesgelinger/Projects/rover/packages/rover-mechanic/target/debug/deps/librover_mechanic-fd802ffb9f5f9087.rmeta: src/lib.rs
-
-/Users/thalesgelinger/Projects/rover/packages/rover-mechanic/target/debug/deps/rover_mechanic-fd802ffb9f5f9087.d: src/lib.rs
-
-src/lib.rs:
