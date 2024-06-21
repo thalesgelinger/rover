d8af49451e0259388cbce51fddd0b8ce5aad03a1
Thales Gelinger <thalesgelinger@gmail.com>
2024-06-21 08:53:51 -0300

    feat: read lua from rust

--- packages/rover-mechanic/src/lib.rs
+++ packages/rover-mechanic/src/lib.rs
@@ -1,5 +1,33 @@
-pub fn gretting(name: &str) -> String {
-    format!("Hello {}", name)
+use std::fs;
+
+use mlua::Lua;
+
+#[no_mangle]
+pub extern "C" fn gretting(name: String) -> String {
+    return lua_exec(name);
+}
+
+fn lua_exec(name: String) -> String {
+    let file_path = "../rover/init.lua";
+    let lua_script = fs::read_to_string(file_path).expect("Unable to read Lua script file");
+
+    let lua = Lua::new();
+
+    let _ = lua
+        .load(&lua_script)
+        .exec()
+        .expect("Error loading lua script");
+
+    let lua_greetings: mlua::Function = lua
+        .globals()
+        .get("luaGreetings")
+        .expect("Error getting luaGreetings function in lua script");
+
+    let result: String = lua_greetings
+        .call(name)
+        .expect("Error calling lua greeting");
+
+    return result;
 }
 
 #[cfg(test)]
@@ -7,8 +35,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = gretting("Rover");
-        assert_eq!(result, "Hello Rover");
+    fn should_run_lua_script() {
+        let result = gretting("Rover".into());
+        assert_eq!(result, "Hello Rover your answer came from lua, BTW");
     }
 }
