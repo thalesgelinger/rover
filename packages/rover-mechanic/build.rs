b301f4fd6397005cff12bc7d6ca63faec955271e
Thales Gelinger <thalesgelinger@gmail.com>
2024-06-27 09:19:53 -0300

    feat: build android so binary

---
+++ packages/rover-mechanic/build.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+fn main() {
+    println!("cargo:warning=Running build.rs script");
+
+    let ndk_path = env::var("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME not set");
+    let target = env::var("TARGET").unwrap();
+    println!("cargo:warning=NDK Path: {}", ndk_path);
+    println!("cargo:warning=Target: {}", target);
+
+    let lib_path = match target.as_str() {
+        "aarch64-linux-android" => "aarch64-linux-android/lib",
+        "armv7-linux-androideabi" => "arm-linux-androideabi/lib",
+        "i686-linux-android" => "i686-linux-android/lib",
+        "x86_64-linux-android" => "x86_64-linux-android/lib",
+        _ => panic!("Unknown target: {}", target),
+    };
+
+    println!(
+        "cargo:rustc-link-search=native={}/sources/cxx-stl/llvm-libc++/libs/{}",
+        ndk_path, lib_path
+    );
+}
+
