4ad4b7843c7b025fff5696819191212979806ec1
Thales Gelinger <thalesgelinger@Thaless-MacBook-Pro.local>
2025-08-06 13:51:17 -0300

    Improve inspect to serialize inner signals

--- src/signals/utils.lua
+++ src/signals/utils.lua
@@ -1,19 +1,33 @@
 local Utils = {}
 
 function Utils.inspect(table)
-    local function serialize(value, indent)
+    local function serialize(val, indent)
+        local value = Utils.unwrap(val)
         if type(value) == "table" then
             local result = "{\n"
             for k, v in pairs(value) do
-                result = result .. indent .. "  [" .. tostring(k) .. "] = " .. serialize(v, indent .. "  ") .. ",\n"
+                result = result .. indent .. "  " .. tostring(k) .. " = " .. serialize(v, indent .. "  ") .. ",\n"
             end
             return result .. indent .. "}"
         else
-            return tostring(value)
+            return tostring(val)
         end
     end
 
-    return serialize(table, "")
+    local jsonString = serialize(table, "")
+
+    for line in jsonString:gmatch("([^\n]+)") do
+        io.write(line .. "\r\n")
+    end
+end
+
+--- @param signal Signal | any
+function Utils.unwrap(signal)
+    if type(signal) == "table" and signal.get then
+        return signal.get()
+    else
+        return signal
+    end
 end
 
 return Utils
