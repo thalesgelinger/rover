f1a107678a66e06012b5e490f57238ab06c0767a
Thales Gelinger <thalesgelinger@Thaless-MacBook-Pro.local>
2025-08-06 13:50:11 -0300

    Add inspect debug capabilities

--- src/signals/screen.lua
+++ src/signals/screen.lua
@@ -1,5 +1,11 @@
 local rover = require("rover")
 local read_key = require("events").read_key
+local unwrap = require("utils").unwrap
+local inspect = require("utils").inspect
+
+
+local gridchar = " "
+
 --- @class Opts
 --- @field debug boolean
 
@@ -8,51 +14,12 @@ local read_key = require("events").read_key
 function UI(app, opts)
     os.execute("clear")
 
-    local gridchar = " "
-
     os.execute("stty raw -echo")
     rover.effect(function()
         io.write("\27[2J\27[H")
-        local c = app.get()
-        local height = c.height or 10 -- Ensure a default value for height
-        local width = c.width or 30   -- Ensure a default value for width
-        for i = 1, height do
-            for j = 1, width do
-                local drawn = false
-                local function render_component(comp)
-                    if comp.x and comp.y then
-                        if j == comp.x and i == comp.y then
-                            io.write(comp[1])
-                            return true
-                        end
-                    end
-                    if #comp >= 1 then
-                        for _, child in ipairs(comp) do
-                            if render_component(child) then
-                                return true
-                            end
-                        end
-                    end
-                    return false
-                end
-
-                for _, comp in ipairs(c) do
-                    if render_component(comp) then
-                        drawn = true
-                        break
-                    end
-                end
-                if not drawn then
-                    io.write(gridchar)
-                end
-                io.write(gridchar)
-            end
-            io.write("\r\n")
-        end
+        Render(app)
         if opts.debug then
-            for _, comp in ipairs(c) do
-                io.write("Component: ( x: ", comp.x, ", y: ", comp.y, ")\r\n")
-            end
+            inspect(app.get())
             io.write("Use arrow keys to move, 'q' to quit\r\n")
         end
         read_key()
@@ -60,6 +27,48 @@ function UI(app, opts)
     end)
 
     os.execute("stty sane")
+end
+
+function Render(app)
+    local c = app.get()
+    local height = c.height or 10 -- Ensure a default value for height
+    local width = c.width or 30   -- Ensure a default value for width
+    for i = 1, height do
+        for j = 1, width do
+            local drawn = false
+            local function render_component(compSignal)
+                local comp = unwrap(compSignal)
+                local x, y = unwrap(comp.x), unwrap(comp.y)
+
+                if x and y then
+                    if j == x and i == y then
+                        io.write(unwrap(comp[1]))
+                        return true
+                    end
+                end
+                if #comp >= 1 then
+                    for _, child in ipairs(comp) do
+                        if render_component(child) then
+                            return true
+                        end
+                    end
+                end
+                return false
+            end
+
+            for _, comp in ipairs(c) do
+                if render_component(comp) then
+                    drawn = true
+                    break
+                end
+            end
+            if not drawn then
+                io.write(gridchar)
+            end
+            io.write(gridchar)
+        end
+        io.write("\r\n")
+    end
 end
 
 return {
