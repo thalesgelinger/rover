5ef457c6115c75a031b4d72bb1335993e5499d1c
Thales Gelinger <thalesgelinger@Thaless-MacBook-Pro.local>
2025-08-06 13:50:02 -0300

    View is a single signal for this sample

--- src/signals/rover.lua
+++ src/signals/rover.lua
@@ -15,35 +15,8 @@ local Rover = {
 --- @property[1] string The symbol to display, can be a string or a table of strings
 --- @return Signal
 function Rover.view(props)
-    local function parseSignalsToStatic(values)
-        local staticValues = {}
-
-        for key, value in pairs(values) do
-            print(key, value)
-
-            if type(value) == "table" and value.get then
-                staticValues[key] = value.get()
-            else
-                staticValues[key] = value
-            end
-
-            inspect(staticValues)
-        end
-        return staticValues
-    end
-
     return Rover.derive(function()
-        local parsedView = parseSignalsToStatic(props)
-        for _, value in ipairs(props) do
-            local child = type(value) == "table" and value.get() or value
-            if type(child) == "string" then
-                table.insert(parsedView, child)
-            elseif type(child) == "table" then
-                table.insert(parsedView, parseSignalsToStatic(child))
-            end
-        end
-
-        return parsedView
+        return props
     end)
 end
 
