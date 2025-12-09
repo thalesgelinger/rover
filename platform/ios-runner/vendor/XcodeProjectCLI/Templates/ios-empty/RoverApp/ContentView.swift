d9494af488efa056dc9f24b6bd1167971aafc913
Thales Gelinger <thalesgelinger@gmail.com>
2025-12-09 17:47:42 -0300

    chore: add ios sim template

---
+++ platform/ios-runner/vendor/XcodeProjectCLI/Templates/ios-empty/RoverApp/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Rover placeholder")
+                .font(.title)
+            Text("Lua/Skia pipeline pending")
+                .font(.subheadline)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ platform/ios-runner/vendor/XcodeProjectCLI/Templates/ios-empty/RoverApp/RoverAppApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct RoverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
