4dcefbc384b7d6f70d7ab4a8b55f2ab1b60755d3
Thales Gelinger <thalesgelinger@gmail.com>
2025-12-15 12:51:40 -0300

    Setup base lua read test

--- rover_core/src/lib.rs
+++ rover_core/src/lib.rs
@@ -1,5 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Result;
+
+pub fn run(path: &str) -> Result<()> {
+    Ok(())
 }
 
 #[cfg(test)]
@@ -7,8 +9,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn should_read_and_print_lua_file() {
+        let result = run("../examples/hello.lua");
+        assert_eq!(result.unwrap(), ());
     }
 }
