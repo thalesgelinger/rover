a5b33da0e30a261d754188d4f7523c4b48a8f46d
Thales Gelinger <thalesgelinger@gmail.com>
2025-12-15 13:51:48 -0300

    Add base cli setup for lua read

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,33 @@
-use rover_core::add;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use rover_core::run;
 
-fn main() {
-    let value = add(1, 1);
-    println!("Hello, world! {value}");
+#[derive(Parser)]
+#[command(name = "rover")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// its just a sample for other commands
+    Sample,
+    #[command(external_subcommand)]
+    External(Vec<String>),
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Sample => {
+            println!("Just a sample cmd");
+            Ok(())
+        }
+        Commands::External(args) => {
+            let path = args.first().unwrap();
+            run(path)
+        }
+    }
 }
