dc616968e7f81f4ef2ad20dc1d1497820b41526f
Thales Gelinger <thalesgelinger@gmail.com>
2025-12-18 18:10:05 -0300

    Add sample with json helper

--- rover_core/examples/complex_server.lua
+++ rover_core/examples/complex_server.lua
@@ -1,31 +1,40 @@
+
 local api = rover.server {}
 
 function api.hello.get(ctx)
-    return {
+    local token = ctx:headers()["Authorization"]
+
+    if not token then
+        return api.json:status(401) {
+            message = "Hello World"
+        }
+    end
+
+    return api.json:status(200) {
         message = "Hello World"
     }
 end
 
 function api.hello.world.get(ctx)
-    return {
+    return api.json{
         message = "Hello World Nested"
     }
 end
 
 function api.hello.world.post(ctx)
-    return {
+    return api.json {
         message = "Post to Hello World"
     }
 end
 
 function api.users.get(ctx)
-    return {
+    return api.json{
         users = {}
     }
 end
 
 function api.users.create.post(ctx)
-    return {
+    return api.json{
         created = true
     }
 end
